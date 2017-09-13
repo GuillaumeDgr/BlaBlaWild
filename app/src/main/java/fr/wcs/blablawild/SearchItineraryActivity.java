a80820c424e63228a4e48192830cfe2d58c03820
GuillaumeDgr <dangerguillaume@gmail.com>
2017-09-13 11:27:40 +0200

    adapter commit

--- app/src/main/java/fr/wcs/blablawild/SearchItineraryActivity.java
+++ app/src/main/java/fr/wcs/blablawild/SearchItineraryActivity.java
@@ -44,5 +44,6 @@ public class SearchItineraryActivity extends AppCompatActivity {
                 }
             }
         });
+
     }
 }
--- app/src/main/java/fr/wcs/blablawild/ViewSearchItineraryResultsListActivity.java
+++ app/src/main/java/fr/wcs/blablawild/ViewSearchItineraryResultsListActivity.java
@@ -3,25 +3,43 @@ package fr.wcs.blablawild;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ListView;
 
-import static fr.wcs.blablawild.R.id.textView5;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class ViewSearchItineraryResultsListActivity extends AppCompatActivity {
+
+    TripResultAdapter mResultsAdapter;
+    ListView mListViewResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_search_itinerary_results_list);
 
+        // Intent
         Intent intent = getIntent();
         String message = intent.getStringExtra("lieuDepart");
         String message2 = intent.getStringExtra("lieuDepart2");
-        TextView textView = (TextView) findViewById(R.id.textView4);
-        TextView textView2 = (TextView) findViewById(textView5);
-        textView.setText(message);
-        textView2.setText(message2);
-
         setTitle(message + " >> " + message2);
+
+        // Array List
+        mListViewResults = (ListView) findViewById(R.id.listView);
+        ArrayList<TripResultModel> results = new ArrayList<>();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy-hh:mm");
+
+        try {
+            results.add(new TripResultModel("Bruce", sdf.parse("21/02/2017-15:30"), 15));
+            results.add(new TripResultModel("Clark", sdf.parse("21/02/2017-16:00"), 20));
+            results.add(new TripResultModel("Bary", sdf.parse("21/02/2017-16:30"), 16));
+            results.add(new TripResultModel("Lex", sdf.parse("21/02/2017-17:00"), 40));
+        } catch (ParseException e) {
+        }
+        mResultsAdapter = new TripResultAdapter(this, results);
+
+        mListViewResults.setAdapter(mResultsAdapter);
     }
 }
